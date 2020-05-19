cf066a87eb5cacdf67a8d4977bf359da919eae49
Canyon Duncan <canyon.duncan@duke.edu>
2020-05-19 10:17:41 -0400

    import math module and added square root functionality

--- calculator.py
+++ calculator.py
@@ -1,32 +1,62 @@
 # calculator.py
 
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
-a = int(a)
-b = int(b)
-print("The number you entered is {} and {}".format(a, b))
+import math 
 
-c = input("Enter a command: ")
+def calcInput():
+    a = input("Enter the first number: ")
+    b = input("Enter the second number: ")
+    a = int(a)
+    b = int(b)
+    print("The number you entered is {} and {}".format(a, b))
+    return a, b
 
-if c == 'a':
+def add(a, b):
     print("Add")
     answer = a + b
     print("{} + {} = {}".format(a, b, answer))
     print("Finished")
-elif c == 's':
+    
+def subtract(a, b):
     print("Subtract")
     answer = a - b
     print("{} - {} = {}".format(a, b, answer))
     print("Finished")
-elif c == 'm':
+    
+def multiply(a, b):
     print("Multiply")
     answer = a * b
     print("{} * {} = {}".format(a, b, answer))
     print("Finished")
-elif c == 'd':
+    
+def divide(a, b):
     print("Divide")
     answer = a / b
     print("{} / {} = {}".format(a, b, answer))
     print("Finished")
-else:
-    print("Error: Not a valid command")
+	
+def squareRoot(a):
+	print("Square Root")
+	answer = math.sqrt(a)
+	print("Square root of {} is {}".format(a, answer))
+
+def mathCommand(a, b):
+    c = input("Enter a command: ")
+    if c == 'a':
+        add(a, b)
+    elif c == 's':
+        subtract(a, b)
+    elif c == 'm':
+        multiply(a, b)
+    elif c == 'd':
+        divide(a, b)
+    elif c == 'q':
+    	squareRoot(a)
+    else:
+        print("Error: Not a valid command")
+
+x, y = calcInput()
+mathCommand(x, y)
+
+
+
+
